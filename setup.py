674a0f1a8a49a2fd7757a7ad1789f7613ce364c5
kapoorlab <randomaccessiblekapoor@gmail.com>
2022-03-20 19:05:22 +0100

    oneat version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md') as f:
 setup(
     name="oneat",
 
-    version='1.0.0',
+    version='1.0.2',
 
     author='Varun Kapoor',
     author_email='randomaccessiblekapoor@gmail.com',
