7d2a982934eac23e832fe49c89785eb327311f60
kapoorlab <randomaccessiblekapoor@gmail.com>
2022-04-11 13:07:51 +0200

    center oneat static

--- oneat/NEATModels/neat_faststandard.py
+++ oneat/NEATModels/neat_faststandard.py
@@ -16,8 +16,8 @@ class NEATSynamic(NEATDynamic):
         super().__init__(config = config, model_dir = model_dir, model_name = model_name, catconfig = catconfig, cordconfig = cordconfig)
         
     def predict_synamic(self,imagename, savedir, n_tiles = (1,1), overlap_percent = 0.8, event_threshold = 0.5, event_confidence = 0.5, iou_threshold = 0.1, fidelity = 5,
-     downsamplefactor = 1, erosion_iterations = 10,start_project_mid = 4, end_project_mid = 4, maskmodel = None, segdir = None, normalize = True):
+     downsamplefactor = 1, erosion_iterations = 10,start_project_mid = 4, end_project_mid = 4, maskmodel = None, segdir = None, normalize = True, center_oneat = True):
 
         self.predict(imagename,savedir,n_tiles = n_tiles, overlap_percent = overlap_percent, event_threshold = event_threshold, event_confidence = event_confidence, iou_threshold = iou_threshold, 
         fidelity = fidelity, start_project_mid = start_project_mid, end_project_mid = end_project_mid, downsamplefactor = downsamplefactor, erosion_iterations = erosion_iterations,  markers = None, marker_tree = None,
-         remove_markers = None, maskmodel = maskmodel, segdir = segdir, normalize = normalize )
+         remove_markers = None, maskmodel = maskmodel, segdir = segdir, normalize = normalize, center_oneat = center_oneat )
--- oneat/NEATModels/neat_standard.py
+++ oneat/NEATModels/neat_standard.py
@@ -8,20 +8,11 @@ class NEATSDynamic(NEATDynamic):
 
                 super().__init__(config = config, model_dir = model_dir, model_name = model_name, catconfig = catconfig, cordconfig = cordconfig)
 
-    
-
-
     def predict_standard(self, imagename, markers, marker_tree, savedir, n_tiles=(1, 1), overlap_percent=0.8,
-                event_threshold=0.5, iou_threshold=0.1, fidelity = 5, downsamplefactor = 1, watershed = None, maskimage = None, maskfilter = 10, normalize = True):
+                event_threshold=0.5, iou_threshold=0.1, fidelity = 5, downsamplefactor = 1, watershed = None, maskimage = None, maskfilter = 10, normalize = True, center_oneat = True):
 
         self.predict(imagename,savedir,n_tiles = n_tiles, overlap_percent = overlap_percent, event_threshold = event_threshold, iou_threshold = iou_threshold, 
         fidelity = fidelity, downsamplefactor = downsamplefactor,  maskfilter = maskfilter, markers = markers, marker_tree = marker_tree, watershed = watershed, maskimage = maskimage,
-         remove_markers = None, normalize = normalize )
+         remove_markers = None, normalize = normalize, center_oneat = center_oneat )
 
-def CreateVolume(patch, imaget, timepoint):
-    starttime = timepoint
-    endtime = timepoint + imaget
-    smallimg = patch[starttime:endtime, :]
-
-    return smallimg
 
