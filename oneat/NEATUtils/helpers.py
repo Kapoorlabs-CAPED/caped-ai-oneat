01f4cfca57072ab790299eed1e1ab0c0dad2ed0c
kapoorlab <randomaccessiblekapoor@gmail.com>
2022-04-11 18:26:54 +0200

    location image

--- oneat/NEATUtils/helpers.py
+++ oneat/NEATUtils/helpers.py
@@ -1441,7 +1441,7 @@ def predictionloop(j, k, sx, sy, nboxes, stride, time_prediction, config, key_ca
             if nearest_location is not None:
                ycentermean, xcentermean = nearest_location
         if center_oneat:
-            if xcenterrawmean > 0.4 and xcenterrawmean < 0.6 and ycenterrawmean > 0.4 and ycenterrawmean < 0.6 and tcenterrawmean > 0.4 and tcenterrawmean < 0.6:       
+            if xcenterrawmean > 0.45 and xcenterrawmean < 0.55 and ycenterrawmean > 0.45 and ycenterrawmean < 0.55 and tcenterrawmean > 0.45 and tcenterrawmean < 0.55:       
                     box = {'xstart': xstart, 'ystart': ystart, 'tstart': boxtstartmean, 'xcenterraw': xcenterrawmean,
                             'ycenterraw': ycenterrawmean, 'tcenterraw': tcenterrawmean, 'xcenter': xcentermean,
                             'ycenter': ycentermean, 'real_time_event': real_time_event, 'box_time_event': box_time_event,
@@ -1465,7 +1465,7 @@ def predictionloop(j, k, sx, sy, nboxes, stride, time_prediction, config, key_ca
                 if nearest_location is not None:
                     ycentermean, xcentermean = nearest_location
             if center_oneat:
-                if xcenterrawmean > 0.4 and xcenterrawmean < 0.6 and ycenterrawmean > 0.4 and ycenterrawmean < 0.6:       
+                if xcenterrawmean > 0.45 and xcenterrawmean < 0.55 and ycenterrawmean > 0.45 and ycenterrawmean < 0.55:       
                         box = {'xstart': xstart, 'ystart': ystart, 'tstart': boxtstartmean, 'xcenterraw': xcenterrawmean,
                                 'ycenterraw': ycenterrawmean, 'tcenterraw': tcenterrawmean, 'xcenter': xcentermean,
                                 'ycenter': ycentermean, 'real_time_event': real_time_event, 'box_time_event': box_time_event,
