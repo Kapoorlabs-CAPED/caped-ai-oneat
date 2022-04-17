2579d6b3cf453ad1cd0e3c8bea7e41a54d30ab3b
kapoorlab <randomaccessiblekapoor@gmail.com>
2022-04-17 15:07:54 +0200

    finall

--- oneat/NEATModels/neat_goldstandard.py
+++ oneat/NEATModels/neat_goldstandard.py
@@ -465,7 +465,7 @@ class NEATDynamic(object):
                                 #nms over time
                                 self.nms()
                                 classedboxes =  self.iou_classedboxes 
-                                if inputtime > 0 and inputtime%(self.imaget) == 0:
+                                if inputtime > 0 and inputtime%(self.imaget * 2) == 0:
                                     
                                     self.nms()
                                     self.to_csv()
@@ -661,7 +661,7 @@ class NEATDynamic(object):
                 self.nms()
                 classedboxes =  self.iou_classedboxes 
                 
-                if inputtime > 0 and inputtime%(self.imaget) == 0:                         
+                if inputtime > 0 and inputtime%(self.imaget * 2) == 0:                         
                         self.nms()
                         self.to_csv()
                         eventboxes = []
