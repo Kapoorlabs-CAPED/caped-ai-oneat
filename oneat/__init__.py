17a62810c0a843e3c62b3c97d44e11e18b31831d
kapoorlab <randomaccessiblekapoor@gmail.com>
2022-04-26 01:08:12 +0200

    xenopus example

--- oneat/__init__.py
+++ oneat/__init__.py
@@ -23,8 +23,14 @@ register_model(NEATSynamic,   'Cellsplitdetectorhdpc',  'https://zenodo.org/reco
                                 'Cellsplitcategorieshelaflou'  , 'https://zenodo.org/record/6481021/files/Cellsplitcategorieshelaflou.json', '7a67a83f08fb1add3c1b1a3e0eeec773',
                                  'Cellsplitdetectorhdpc_Parameter' , 'https://zenodo.org/record/6483483/files/Cellsplitdetectorhdpc_Parameter.json', '556f0ce063a4cacda6b9f27ae7aae69b'  )  
 
+register_model(NEATSynamic,   'Cellsplitdetectorxenopus',  'https://zenodo.org/record/6484966/files/Cellsplitdetectorxenopus.h5', '299a2edebf217da76732fd812cb5d6fe',
+                               'Cellsplitcordxenopus'         ,   'https://zenodo.org/record/6484966/files/Cellsplitcordxenopus.json', 'aed21cb69d6fb8be32c47f78a39d32f5',
+                                'Cellsplitcategoriesxenopus'  , 'https://zenodo.org/record/6484966/files/Cellsplitcategoriesxenopus.json', '7a67a83f08fb1add3c1b1a3e0eeec773',
+                                 'Cellsplitdetectorxenopus_Parameter' , 'https://zenodo.org/record/6484966/files/Cellsplitdetectorxenopus_Parameter.json', 'ee3dfb4ff1af80e44ceaebbdf4b1bff1'  )                                  
+
 register_aliases(NEATSynamic, 'Cellsplitdetectorbrightfield',  'Cellsplitdetectorbrightfield')
 register_aliases(NEATSynamic, 'Cellsplitdetectorhdpc',  'Cellsplitdetectorhdpc')
+register_aliases(NEATSynamic, 'Cellsplitdetectorxenopus',  'Cellsplitdetectorxenopus')
 
 
 del register_model, register_aliases, clear_models_and_aliases
@@ -58,3 +64,14 @@ def test_image_hdpc(target):
     Name = os.path.basename(os.path.splitext(fname)[0])
     imwrite(target + '/' + Name + '.tif', image.astype('float32'))
     return fname  
+
+def test_image_xenopus(target):
+    from tifffile import imread, imwrite
+    import os
+    url = "https://zenodo.org/record/6484966/files/C1-for_oneat_prediction.tif"
+    hash = "ccf1836e66c350f90a3f458890d16220"
+    fname = abspath(get_file(fname='xenopus', origin=url, file_hash=hash))
+    image = imread(fname)
+    Name = os.path.basename(os.path.splitext(fname)[0])
+    imwrite(target + '/' + Name + '.tif', image.astype('float32'))
+    return fname     
