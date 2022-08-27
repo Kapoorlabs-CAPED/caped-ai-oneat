8684b2fb54b4710fb5b4d0fb20deb8bf3083f043
kapoorlab <randomaccessiblekapoor@gmail.com>
2022-08-27 16:23:11 +0200

    augment zt

--- oneat/NEATUtils/__init__.py
+++ oneat/NEATUtils/__init__.py
@@ -2,5 +2,5 @@ from __future__ import absolute_import, print_function
 from .HolovizNapari import NEATViz 
 from .Zmapgen import Zmapgen
 from .TemporalAug import TemporalAug
-
+from .TemporalZAug import TemporalZAug
 
