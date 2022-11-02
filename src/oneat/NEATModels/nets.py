c9219bdab3e696d5628b982c0d7d7dabd5c52d36
kapoorlab <randomaccessiblekapoor@gmail.com>
2022-11-02 14:39:51 +0100

    added dense blocks

--- src/oneat/NEATModels/nets.py
+++ src/oneat/NEATModels/nets.py
@@ -1,5 +1,5 @@
 import keras as K
-from keras import layers, models, regularizers
+from keras import layers, models, regularizers, backend
 from keras.layers import (
     Activation,
     BatchNormalization,
@@ -1235,6 +1235,73 @@ def resnet_layer_1D(
             x = Activation(activation)(x)
         x = conv(x)
     return x
+
+
+
+def dense_block(x, blocks, name):
+    """A dense block.
+    Args:
+      x: input tensor.
+      blocks: integer, the number of building blocks.
+      name: string, block label.
+    Returns:
+      Output tensor for the block.
+    """
+    for i in range(blocks):
+        x = dense_conv_block(x, 32, name=name + "_block" + str(i + 1))
+    return x
+
+
+def transition_block(x, reduction, name):
+    """A transition block.
+    Args:
+      x: input tensor.
+      reduction: float, compression rate at transition layers.
+      name: string, block label.
+    Returns:
+      output tensor for the block.
+    """
+    bn_axis = -1 if backend.image_data_format() == "channels_last" else 1
+    x = layers.BatchNormalization(
+        axis=bn_axis, epsilon=1.001e-5, name=name + "_bn"
+    )(x)
+    x = layers.Activation("relu", name=name + "_relu")(x)
+    x = layers.Conv3D(
+        int(backend.int_shape(x)[bn_axis] * reduction),
+        1,
+        use_bias=False,
+        name=name + "_conv",
+    )(x)
+    x = layers.AveragePooling2D(2, strides=2, name=name + "_pool")(x)
+    return x
+
+
+def dense_conv_block(x, growth_rate, name):
+    """A building block for a dense block.
+    Args:
+      x: input tensor.
+      growth_rate: float, growth rate at dense layers.
+      name: string, block label.
+    Returns:
+      Output tensor for the block.
+    """
+    bn_axis = -1 if backend.image_data_format() == "channels_last" else 1
+    x1 = layers.BatchNormalization(
+        axis=bn_axis, epsilon=1.001e-5, name=name + "_0_bn"
+    )(x)
+    x1 = layers.Activation("relu", name=name + "_0_relu")(x1)
+    x1 = layers.Conv3D(
+        4 * growth_rate, 1, use_bias=False, name=name + "_1_conv"
+    )(x1)
+    x1 = layers.BatchNormalization(
+        axis=bn_axis, epsilon=1.001e-5, name=name + "_1_bn"
+    )(x1)
+    x1 = layers.Activation("relu", name=name + "_1_relu")(x1)
+    x1 = layers.Conv3D(
+        growth_rate, 3, padding="same", use_bias=False, name=name + "_2_conv"
+    )(x1)
+    x = layers.Concatenate(axis=bn_axis, name=name + "_concat")([x, x1])
+    return 
 
 
 def resnet_3D_layer(
