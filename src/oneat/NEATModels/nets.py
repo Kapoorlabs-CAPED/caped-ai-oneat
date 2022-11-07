07677f4f6850215091fce8a73026a221850353ac
kapoorlab <randomaccessiblekapoor@gmail.com>
2022-11-07 14:42:05 +0100

    iclass structure for dense and vollnet

--- src/oneat/NEATModels/nets.py
+++ src/oneat/NEATModels/nets.py
@@ -197,219 +197,111 @@ def LRNet(
     return model
 
 
-def VollNet(
-    input_shape,
-    categories,
-    box_vector,
-    nboxes=1,
-    stage_number=3,
-    depth=38,
-    start_kernel=7,
-    mid_kernel=3,
-    startfilter=32,
-    input_weights=None,
-    last_activation="softmax",
-):
 
-    img_input = layers.Input(shape=(None, None, None, input_shape[3]))
-    if (depth - 2) % 9 != 0:
-        raise ValueError("depth should be 9n+2 (eg 56 or 110 in [b])")
-    # Start model definition.
-    num_filters_in = startfilter
-    num_res_blocks = int((depth - 2) / 9)
-    last_conv_factor = 2 ** (stage_number - 1)
-    # v2 performs Conv2D with BN-ReLU on input before splitting into 2 paths
-    x = resnet_3D_layer(
-        inputs=img_input,
-        num_filters=num_filters_in,
-        kernel_size=start_kernel,
-        conv_first=True,
+    
+    
+    
+    
+def _voll_dense_block(x, nb_layers,
+    num_filters,
+    kernel_size,
+    activation):
+    
+    for _ in range(nb_layers):
+        x = _voll_dense_conv(x, num_filters, kernel_size,activation)
+    return x    
+    
+def _voll_transition_block(x,
+                           reduction,
+                           activation="relu"):
+    
+    x = BatchNormalization()(x)
+    x = Activation(activation)(x)
+    x = Conv3D(
+        int(backend.int_shape(x)[-1] * reduction),
+        1,
+        use_bias=False
+    )(x)
+    
+    x = layers.MaxPooling3D((2, 2, 2))(x)
+    
+    return x
+    
+def _voll_dense_conv(
+    x,
+    num_filters,
+    kernel_size=3,
+    activation="relu"
+):
+    
+    
+    y = BatchNormalization()(x)
+    y = Activation(activation)(y)
+    y = Conv3D(
+        4 * num_filters,
+        kernel_size= 1,
+        use_bias = False,
+        padding="same",
+        kernel_initializer="he_normal",
+        kernel_regularizer=regularizers.l2(reg_weight),
+    ) (y)
+    y = BatchNormalization()(y)
+    y = Activation(activation)(y)
+    
+    y = Conv3D(
+        num_filters,
+        kernel_size= kernel_size,
+        use_bias = False,
+        padding="same",
+        kernel_initializer="he_normal",
+        kernel_regularizer=regularizers.l2(reg_weight),
+    ) (y)
+    
+    block = Concat(-1)
+    x = block([y, x])
+    
+    return x    
+    
+def _voll_conv(
+    inputs,
+    num_filters=64,
+    kernel_size=3,
+    strides=1,
+    activation="relu",
+    batch_normalization=True,
+    conv_first=True,
+):
+    
+    conv = Conv3D(
+        num_filters,
+        kernel_size=kernel_size,
+        strides= strides,
+        padding="same",
+        kernel_initializer="he_normal",
+        kernel_regularizer=regularizers.l2(1e-4),
     )
 
-    # Instantiate the stack of residual units
-    for stage in range(stage_number):
-        for res_block in range(num_res_blocks):
-            activation = "relu"
-            batch_normalization = True
-            strides = 1
-            if stage == 0:
-                num_filters_out = num_filters_in * 4
-                if res_block == 0:  # first layer and first stage
-                    activation = None
-                    batch_normalization = False
-            else:
-                num_filters_out = num_filters_in * 2
-                if res_block == 0:  # not first layer and not first stage
-                    strides = 2  # downsample
+    x = inputs
+    if conv_first:
+        x = conv(x)
+        if batch_normalization:
+            x = BatchNormalization()(x)
+        if activation is not None:
+            x = Activation(activation)(x)
+    else:
+        if batch_normalization:
+            x = BatchNormalization()(x)
+        if activation is not None:
+            x = Activation(activation)(x)
+        x = conv(x)
+    return x
 
-            y = resnet_3D_layer(
-                inputs=x,
-                num_filters=num_filters_in,
-                kernel_size=1,
-                strides=strides,
-                activation=activation,
-                batch_normalization=batch_normalization,
-                conv_first=False,
-            )
-            y = resnet_3D_layer(
-                inputs=y,
-                num_filters=num_filters_in,
-                kernel_size=mid_kernel,
-                conv_first=False,
-            )
-            y = resnet_3D_layer(
-                inputs=y,
-                num_filters=num_filters_out,
-                kernel_size=1,
-                conv_first=False,
-            )
-            if res_block == 0:
-                # linear projection residual shortcut connection to match
-                # changed dims
-                x = resnet_3D_layer(
-                    inputs=x,
-                    num_filters=num_filters_out,
-                    kernel_size=1,
-                    strides=strides,
-                    activation=None,
-                    batch_normalization=False,
-                )
 
-            x = K.layers.add([x, y])
-        num_filters_in = num_filters_out
-    x = (
-        Conv3D(
-            categories + nboxes * box_vector,
-            kernel_size=mid_kernel,
-            kernel_regularizer=regularizers.l2(reg_weight),
-            padding="same",
-        )
-    )(x)
-    x = BatchNormalization()(x)
-    x = Activation("relu")(x)
-    input_cat = Lambda(lambda x: x[:, :, :, :, 0:categories])(x)
-    input_box = Lambda(lambda x: x[:, :, :, :, categories:])(x)
-
-    output_cat = (
-        Conv3D(
-            categories,
-            (
-                round(input_shape[0] ),
-                round(input_shape[1] / last_conv_factor),
-                round(input_shape[2] / last_conv_factor ),
-            ),
-            activation=last_activation,
-            kernel_regularizer=regularizers.l2(reg_weight),
-            padding="valid",
-            name="yolo",
-        )
-    )(input_cat)
-    output_box = (
-        Conv3D(
-            nboxes * (box_vector),
-            (
-                round(input_shape[0]),
-                round(input_shape[1] / last_conv_factor),
-                round(input_shape[2] / last_conv_factor),
-            ),
-            activation="sigmoid",
-            kernel_regularizer=regularizers.l2(reg_weight),
-            padding="valid",
-            name="secyolo",
-        )
-    )(input_box)
-
-    block = Concat(-1)
-    outputs = block([output_cat, output_box])
-
-    inputs = img_input
-
-    # Create model.
-    model = models.Model(inputs, outputs)
-
-    if input_weights is not None:
-
-        model.load_weights(input_weights, by_name=True)
-
-    return model
-class DenseNet:
-    def __init__(self, stage_number, growth_rate, start_kernel = 7, mid_kernel = 3,reduction = 0.5):
-        self.stage_number = stage_number
-        self.growth_rate = growth_rate
-        self.start_kernel = start_kernel
-        self.mid_kernel = mid_kernel
-        self.reduction = reduction
-
-    def __call__(self, x):
-        if (self.reduction != 1.0) :
-            channels = self.growth_rate * 2
-        else:
-            channels = 16
-        x = self.first_conv3d(x, channels)
-        for i, n_blocks in enumerate(self.stage_number):
-            if i != 0:
-                x = self.transition_layer(x)
-            x = self.dense_block(x, n_blocks)
-       
-        return x
-
-    def first_conv3d(self, x, channels):
-        kernel_size =  self.start_kernel
-        x = self._conv3d(x, channels, kernel_size)
-        
-        return x
-
-    def convolution_block(self, x):
-        
-        return self.bn_relu_conv3d(x, self.growth_rate, self.mid_kernel)
-
-    def dense_block(self, x, n_blocks):
-        for _ in range(n_blocks):
-            x = self._dense_block(x)
-        return x
-
-    def _dense_block(self, x):
-        bypass = self.convolution_block(x)
-        return layers.Concatenate()([x, bypass])
-
-    def transition_layer(self, x):
-        output_channels = int(x.shape[-1] * self.reduction)
-        x = self.bn_relu_conv3d(x, output_channels, 1)
-        return layers.MaxPooling3D((2, 2, 2))(x)
-
-    def bn_relu_conv3d(self, x, output_channels, kernel):
-        
-        return self._conv3d(x, output_channels, kernel)
-
-    def _conv3d(self, x, output_channels, kernel, padding="same"):
-        x = layers.Conv3D(output_channels, kernel, padding=padding, kernel_regularizer=regularizers.l2(reg_weight))(x)
-        x = layers.BatchNormalization()(x)
-        x = layers.Activation('relu')(x)
-        
-        return x
-def DenseVollNet(
-                input_shape,
-                categories: dict,
-                box_vector,
-                nboxes: int=1,
-                start_kernel: int=7,
-                mid_kernel: int=3,
-                startfilter: int=32,
-                stage_number: int = 3,
-                input_weights=None,
-                last_activation: str="softmax",
-                depth: int=40,
-                growth_rate: int=32,
-                nb_filter: int=-1,
-                nb_layers_per_block: dict = {'depth_0': 6, 'depth_1': 12, 'depth_2':24},
-                reduction: float = 0.5
-):
+def _voll_top(input_shape, depth, stage_number):
     
-    
-        # layers in each dense block
         nb_layers = []
-        if type(nb_layers_per_block) is dict:
-            for (k,v) in nb_layers_per_block.items():
+        if type(depth) is dict:
+            for (k,v) in depth.items():
                 nb_layers.append(v) # get the list
 
             if len(nb_layers) != stage_number:
@@ -420,8 +312,11 @@ def DenseVollNet(
         last_conv_factor = 2 ** (stage_number - 1) 
         print(input_shape, input_shape[0], input_shape[1], input_shape[2], input_shape[3])
         img_input = layers.Input(shape=(None, None, None, input_shape[3]))
-        densenet = DenseNet(nb_layers, growth_rate = growth_rate, start_kernel = start_kernel, mid_kernel = mid_kernel, reduction = reduction)
-        x = densenet(img_input)
+        
+        return last_conv_factor, img_input
+
+def _voll_bottom(x, img_input, input_shape, categories, last_conv_factor, last_activation, nboxes, box_vector, input_weights):
+        
         input_cat = Lambda(lambda x: x[:, :, :, :, 0:categories])(x)
         input_box = Lambda(lambda x: x[:, :, :, :, categories:])(x)
         output_cat = (
@@ -464,12 +359,165 @@ def DenseVollNet(
             model.load_weights(input_weights, by_name=True)
 
         return model
+
+class DenseNet:
+    def __init__(self, depth, startfilter, stage_number, start_kernel, mid_kernel,reduction, activation='relu'):
+        self.stage_number = stage_number
+        self.start_kernel = start_kernel
+        self.mid_kernel = mid_kernel
+        self.reduction = reduction
+        self.depth = depth
+        self.startfilter = startfilter
+        self.activation = activation
+
+    def __call__(self, x):
+        
+        self.nb_layers = []
+        if type(self.depth) is dict:
+            for (k,v) in self.depth.items():
+                self.nb_layers.append(v) # get the list
+                
+        if len(self.nb_layers) != self.stage_number:
+                raise ValueError('If `stage_number` is a list, its length must match '
+                                 'the number of layers provided by `depth`.')
+        for stage in range(self.stage_number):
+            
+            x = _voll_dense_block(x, self.nb_layers[stage],
+                self.startfilter//2,
+                self.mid_kernel,
+                self.activation)
+            if stage < self.stage_number:
+               x =_voll_transition_block(x,
+                           self.reduction,
+                           self.activation)
+               
+            x = BatchNormalization()(x)
+            x = Activation(self.activation)(x)   
+        
+        return x
+class ResNet:
+    def __init__(self, depth, startfilter, stage_number, start_kernel, mid_kernel, activation='relu'):
+        
+        self.depth = depth
+        self.startfilter = startfilter
+        self.stage_number = stage_number
+        self.start_kernel = start_kernel
+        self.mid_kernel = mid_kernel
+        self.activation = activation  
+        
+    def __call__(self, x):
+        
+        self.num_filters_in = self.startfilter
+        if type(self.depth) is dict:
+            for (k,v) in self.depth.items():
+                self.nb_layers = v
+        else:
+                
+              self.nb_layers = self.depth      
+                
+        num_res_blocks = int((self.nb_layers - 2) / 9)
+        x = _voll_conv(
+            x,
+            num_filters=self.startfilter,
+            kernel_size=self.start_kernel,
+            conv_first=True
+        )
+              
+        for stage in range(self.stage_number):
+                for res_block in range(num_res_blocks):
+                    activation = self.activation
+                    batch_normalization = True
+                    strides = 1
+                    if stage == 0:
+                        self.num_filters_out = self.num_filters_in * 4
+                        if res_block == 0:  # first layer and first stage
+                            activation = None
+                            batch_normalization = False
+                    else:
+                        self.num_filters_out = self.num_filters_in * 2
+                        if res_block == 0:  # not first layer and not first stage
+                            strides = 2  # downsample
+
+                    y = _voll_conv(
+                        inputs=x,
+                        num_filters=self.num_filters_in,
+                        kernel_size=1,
+                        strides=strides,
+                        activation=activation,
+                        batch_normalization=batch_normalization,
+                        conv_first=False,
+                    )
+                    y = _voll_conv(
+                        inputs=y,
+                        num_filters=self.num_filters_in,
+                        kernel_size=self.mid_kernel,
+                        conv_first=False,
+                    )
+                    y = _voll_conv(
+                        inputs=y,
+                        num_filters=self.num_filters_out,
+                        kernel_size=1,
+                        conv_first=False,
+                    )
+                    if res_block == 0:
+                        # linear projection residual shortcut connection to match
+                        # changed dims
+                        x = _voll_conv(
+                            inputs=x,
+                            num_filters=self.num_filters_out,
+                            kernel_size=1,
+                            strides=strides,
+                            activation=None,
+                            batch_normalization=False,
+                        )
+
+                    x = K.layers.add([x, y])
+                self.num_filters_in = self.num_filters_out
+                
+        return x        
+                
+def DenseVollNet(
+                input_shape,
+                categories: dict,
+                box_vector,
+                nboxes: int=1,
+                start_kernel: int=7,
+                mid_kernel: int=3,
+                startfilter: int=64,
+                stage_number: int = 3,
+                input_weights=None,
+                last_activation: str="softmax",
+                depth: dict={'depth_0': 6, 'depth_1': 12, 'depth_2':24, 'depth_3':16},
+                reduction: float = 0.5
+):
+    
+        last_conv_factor, img_input = _voll_top(input_shape = input_shape, depth = depth, stage_number = stage_number)
+        densenet = DenseNet(depth, startfilter, stage_number, start_kernel, mid_kernel, reduction)
+        x = densenet(img_input)
+        model = _voll_bottom(x, img_input, input_shape, categories, last_conv_factor, last_activation, nboxes, box_vector, input_weights)
+        return model
         
 
 
-
-
-
+def VollNet(
+            input_shape,
+            categories: dict,
+            box_vector,
+            nboxes: int=1,
+            start_kernel: int=7,
+            mid_kernel: int=3,
+            startfilter: int=64,
+            stage_number: int = 3,
+            input_weights=None,
+            last_activation: str="softmax",
+            depth: dict={'depth_0': 29}
+):
+    
+        last_conv_factor, img_input = _voll_top(input_shape = input_shape, depth = depth, stage_number = stage_number)
+        resnet = ResNet(depth, startfilter, stage_number, start_kernel, mid_kernel)
+        x = resnet(img_input)
+        model = _voll_bottom(x, img_input, input_shape, categories, last_conv_factor, last_activation, nboxes, box_vector, input_weights)
+        return model
 
 
 def resnet_lstm_v2(
@@ -1291,49 +1339,3 @@ def _resnet_layer_1D(
 
 
 
-
-def resnet_3D_layer(
-    inputs,
-    num_filters=64,
-    kernel_size=3,
-    strides=1,
-    activation="relu",
-    batch_normalization=True,
-    conv_first=True,
-):
-    """3D Convolution-Batch Normalization-Activation stack builder
-    # Arguments
-        inputs (tensor): input tensor from input image or previous layer
-        num_filters (int): Conv2D number of filters
-        kernel_size (int): Conv2D square kernel dimensions
-        strides (int): Conv2D square stride dimensions
-        activation (string): activation name
-        batch_normalization (bool): whether to include batch normalization
-        conv_first (bool): conv-bn-activation (True) or
-            bn-activation-conv (False)
-    # Returns
-        x (tensor): tensor as input to the next layer
-    """
-    conv = Conv3D(
-        num_filters,
-        kernel_size=kernel_size,
-        strides= strides,
-        padding="same",
-        kernel_initializer="he_normal",
-        kernel_regularizer=regularizers.l2(1e-4),
-    )
-
-    x = inputs
-    if conv_first:
-        x = conv(x)
-        if batch_normalization:
-            x = BatchNormalization()(x)
-        if activation is not None:
-            x = Activation(activation)(x)
-    else:
-        if batch_normalization:
-            x = BatchNormalization()(x)
-        if activation is not None:
-            x = Activation(activation)(x)
-        x = conv(x)
-    return x
