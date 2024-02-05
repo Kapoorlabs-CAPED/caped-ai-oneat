7fe69e06b9a330c3adbd8f6cfcee05bb656840d2
kapoorlab <varun.kapoor@kapoorlabs.org>
2024-02-05 14:25:59 +0000

    version bumpy

--- src/oneat/NEATUtils/TrainDataMaker.py
+++ src/oneat/NEATUtils/TrainDataMaker.py
@@ -2,112 +2,145 @@ import napari
 import os
 
 from tifffile import imread
-
+import random
 import pandas as pd
 
 
+class TrainDataMaker:
+    def __init__(
+        self,
+        source_dir,
+        create_2dt=False,
+        class_names=["Normal", "Mitosis", "Apoptosis"],
+    ):
 
-class TrainDataMaker(object):
-       def __init__(self, source_dir):
-              
-              self.source_dir = source_dir
-              self.acceptable_formats = [".tif", ".TIFF", ".TIF", ".png"]
-              self._click_maker()
+        self.source_dir = source_dir
+        self.create_2dt = create_2dt
+        self.class_names = class_names
+        self.acceptable_formats = [".tif", ".TIFF", ".TIF", ".png"]
+        self._click_maker()
 
-       def _click_maker(self):
-           from qtpy.QtWidgets import QComboBox, QPushButton
+    def _click_maker(self):
+        from qtpy.QtWidgets import QComboBox, QPushButton
 
-           self.viewer = napari.Viewer()
-           Imageids = []
-           Boxname = 'ImageIDBox'
-           X = os.listdir(self.source_dir)
-           imageidbox = QComboBox()   
-           imageidbox.addItem(Boxname)   
-           tracksavebutton = QPushButton('Save Clicks')
-           for imagename in X:
-                  if any(imagename.endswith(f) for f in self.acceptable_formats):
-                      Imageids.append(os.path.join(self.source_dir,imagename))
-           for i in range(0, len(Imageids)):
-                 imageidbox.addItem(str(Imageids[i])) 
+        self.viewer = napari.Viewer()
+        Imageids = []
+        Boxname = "ImageIDBox"
+        X = os.listdir(self.source_dir)
+        imageidbox = QComboBox()
+        imageidbox.addItem(Boxname)
+        tracksavebutton = QPushButton("Save Clicks")
+        for imagename in X:
+            if any(imagename.endswith(f) for f in self.acceptable_formats):
+                Imageids.append(os.path.join(self.source_dir, imagename))
+        for i in range(0, len(Imageids)):
+            imageidbox.addItem(str(Imageids[i]))
 
-           self.viewer.window.add_dock_widget(imageidbox, name="Image", area='bottom')    
-           self.viewer.window.add_dock_widget(tracksavebutton, name="Save Clicks", area='bottom')
-           imageidbox.currentIndexChanged.connect(
-                        lambda trackid = imageidbox: EventViewer(
-                                self.viewer,
-                                imageidbox.currentText(),
-                                os.path.basename(os.path.splitext(imageidbox.currentText())[0]), self.source_dir, False, True ))     
+        self.viewer.window.add_dock_widget(imageidbox, name="Image", area="bottom")
+        self.viewer.window.add_dock_widget(
+            tracksavebutton, name="Save Clicks", area="bottom"
+        )
+        imageidbox.currentIndexChanged.connect(
+            lambda trackid=imageidbox: EventViewer(
+                self.viewer,
+                imageidbox.currentText(),
+                os.path.basename(os.path.splitext(imageidbox.currentText())[0]),
+                self.source_dir,
+                save=False,
+                newimage=True,
+                create_2dt=self.create_2dt,
+                class_names=self.class_names,
+            )
+        )
 
-           tracksavebutton.clicked.connect(
-                        lambda trackid= tracksavebutton:EventViewer(
-                                self.viewer,
-                                imageidbox.currentText(),
-                                os.path.basename(os.path.splitext(imageidbox.currentText())[0]), self.source_dir, True, False ))                
-                     
-           napari.run()   
+        tracksavebutton.clicked.connect(
+            lambda trackid=tracksavebutton: EventViewer(
+                self.viewer,
+                imageidbox.currentText(),
+                os.path.basename(os.path.splitext(imageidbox.currentText())[0]),
+                self.source_dir,
+                save=True,
+                newimage=False,
+                create_2dt=self.create_2dt,
+                class_names=self.class_names,
+            )
+        )
+
+        napari.run()
 
 
-class EventViewer(object):
+class EventViewer:
+    def __init__(
+        self,
+        viewer: napari.Viewer,
+        imagename,
+        Name,
+        csv_dir,
+        save=False,
+        newimage=True,
+        create_2dt=False,
+        class_names=[],
+    ):
 
-    def __init__(self, viewer, imagename, Name, csv_dir, save = False, newimage = True):
-     
-          self.save = save
-          self.newimage = newimage
-          self.viewer = viewer
-          print('reading image')      
-          self.imagename = imagename  
-          self.image = imread(imagename)
-          print('image read')
-          self.Name = Name
-          self.ndim = len(self.image.shape)  
-          self.csv_dir = csv_dir
-          assert self.ndim == 4, f'Input image should be 4 dimensional, try Training_data_maker for 2D + time images'
-          
-          self._click()
+        self.save = save
+        self.newimage = newimage
+        self.viewer = viewer
+        print("reading image")
+        self.imagename = imagename
+        self.image = imread(imagename)
+        print("image read")
+        self.Name = Name
+        self.ndim = len(self.image.shape)
+        self.csv_dir = csv_dir
+        self.create_2dt = create_2dt
+        self.class_names = class_names
+        self.class_colors = {
+            class_name: self.generate_random_color() for class_name in self.class_names
+        }
+        if not self.create_2dt:
+            assert (
+                self.ndim == 4
+            ), f"Input image should be 4 dimensional, found {self.ndim}, try Training_data_maker for 2D + time images"
+        else:
+            assert (
+                self.ndim == 3
+            ), f"Input image should be 3 dimensional,found {self.ndim}, try contacting KapoorLabs for custom image analysis development"
+
+        self._click()
+
+    def generate_random_color(self):
+        return f"#{random.randint(0, 0xFFFFFF):06x}"
 
     def _click(self):
 
-        
-                ClassBackground = 'Normal'
-                ClassMitosis = 'Mitosis'
-                ClassApoptosis = 'Apoptosis'
-                
-                if self.save == True:
- 
-                        ClassBackgrounddata = self.viewer.layers[ClassBackground].data
-                        bgdf = pd.DataFrame(ClassBackgrounddata, index = None, columns = ['T', 'Z', 'Y', 'X'])
-                        bgdf.to_csv(self.csv_dir + '/' + 'ONEAT' + ClassBackground + self.Name +  '.csv', index = False, mode = 'w')
+        if self.save is True:
+            for class_name in self.class_names:
+                class_data = self.viewer.layers[class_name].data
+                if not self.create_2dt:
+                    class_df = pd.DataFrame(
+                        class_data, index=None, columns=["T", "Z", "Y", "X"]
+                    )
+                else:
+                    class_df = pd.DataFrame(
+                        class_data, index=None, columns=["T", "Y", "X"]
+                    )
+                class_df.to_csv(
+                    self.csv_dir + "/ONEAT" + class_name + self.Name + ".csv",
+                    index=False,
+                    mode="w",
+                )
 
-                        ClassMitosisdata = self.viewer.layers[ClassMitosis].data
-                        divdf = pd.DataFrame(ClassMitosisdata, index = None, columns = ['T', 'Z', 'Y', 'X'])
-                        divdf.to_csv(self.csv_dir + '/' +'ONEAT' + ClassMitosis + self.Name +  '.csv', index = False, mode = 'w')
+        if self.newimage is True:
+            for layer in list(self.viewer.layers):
 
-                        ClassApoptosisdata = self.viewer.layers[ClassApoptosis].data
-                        apopdf = pd.DataFrame(ClassApoptosisdata, index = None, columns = ['T', 'Z', 'Y', 'X'])
-                        apopdf.to_csv(self.csv_dir + '/' +'ONEAT' + ClassApoptosis + self.Name +  '.csv', index = False, mode = 'w')
+                self.viewer.layers.remove(layer)
 
-                       
-        
-                if self.newimage == True:
-                     for layer in list(self.viewer.layers):
+        if self.save is False:
+            self.viewer.add_image(self.image, name=self.Name)
 
-                            self.viewer.layers.remove(layer) 
-                    
-                if self.save == False:
-                        self.viewer.add_image(self.image, name = self.Name)
-
-
-                        # add the first points layer for ClassBackground Z and T point
-                        self.viewer.add_points(name= ClassBackground, face_color='red', ndim = 4)
-
-                        # add the second points layer for ClassMitosis Z and T point
-                        self.viewer.add_points(name= ClassMitosis, face_color='blue', ndim = 4)
-
-                        # add the second points layer for ClassApoptosis Z and T point
-                        self.viewer.add_points(name= ClassApoptosis, face_color='green', ndim = 4)
-                      
-                        # programatically enter add mode for both Points layers to enable editing
-
-                        self.viewer.layers[ClassBackground].mode = 'add'
-                        self.viewer.layers[ClassMitosis].mode = 'add'
-                        self.viewer.layers[ClassApoptosis].mode = 'add'
+            for class_name in self.class_names:
+                face_color = self.class_colors[class_name]
+                self.viewer.add_points(
+                    name=class_name, face_color=face_color, ndim=self.ndim
+                )
+                self.viewer.layers[class_name].mode = "add"
--- src/oneat/_version.py
+++ src/oneat/_version.py
@@ -1,2 +1,2 @@
-__version__ = version = "6.7.6"
-__version_tuple__ = version_tuple = (6, 7, 6)
+__version__ = version = "6.7.7"
+__version_tuple__ = version_tuple = (6, 7, 7)
